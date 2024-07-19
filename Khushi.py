4188808f2af06f3e1b050a894ac64f857d13d9ab
khushi30-coder <mauryakhushboo936@gmail.com>
2024-07-19 13:03:42 +0530

    Create Khushi.py

---
+++ Khushi.py
@@ -0,0 +1,50 @@
+import numpy as np
+print(":two mayrix multiply:")
+a1=np.array([ [1,2,3],[4,5,6]])
+a2=np.array ([[7,8,9],[10,11,12]])
+r=(a1*a2)
+print(r)
+print(":________________________________:")
+import numpy as np
+print(":add two matrix:")
+a1=np.array([ [1,2,3],[4,5,6]])
+a2=np.array ([[7,8,9],[10,11,12]])
+sum=(a1+a2)
+print(sum)
+print(":________________________________:")
+import numpy as np
+print(":add two matrix:")
+a1=np.array([ [1,2,3],[4,5,6]])
+a2=np.array ([[7,8,9],[10,11,12]])
+print("sum of two matric ")
+print(np.add(a1,a2))
+print(":________________________________:")
+print(":traverse matrix :")
+a=[ [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+for r in a:
+    print(r)
+print(":________________________________:")
+import numpy as np
+a=np.array([[2,2,3],
+           [4,5,6],
+           [7,8,9]
+])
+print(":copy of  matrix:")
+b=np.copy(a)
+print(b)
+print(":________________________________:")
+
+import numpy as np
+print(":add two matrix:")
+a1=np.array([ [1,2,3],[4,5,6]])
+a2=np.array ([[7,8,9],[10,11,12]])
+r=a1+a2
+print(r)
+print(":________________________________:")
+
+
+
+
